40c17236eecb9172f1ebf9a76251168615286db7
Juan Carlos Concha <jnkarlos.c@gmail.com>
2021-03-25 15:28:29 -0300

    Rest api to restaurant and services #2

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,10 @@
 class Restaurant < ApplicationRecord
+
+  def self.allowed_attributes_create
+    %i[rating name site email phone street city state lat lng]
+  end
+
+  def self.allowed_attributes_update
+    %i[rating name site email phone street city state lat lng]
+  end
 end
